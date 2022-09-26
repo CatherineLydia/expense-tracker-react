1e41d9fd2e6b5f2a69fd57469ca38995b3208e85
Catherine Lydia <p.cath.reens@gmail.com>
2022-09-26 17:02:05 -0400

    Balance Component

---
+++ src/components/BalanceComponent.js
@@ -0,0 +1,14 @@
+const Balance = () => {
+    return (
+        <>
+            <h1>Expense Tracker</h1>
+            <div>
+                <p>Balance</p>
+                <div>$0</div>
+            </div>
+        </>
+    )
+
+}
+
+export default Balance;
